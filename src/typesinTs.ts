8e7f6be7d79dfe1beea0a0c366a4b14d97d67f5d
Callme-VR <176268004+Callme-VR@users.noreply.github.com>
2025-12-25 00:07:04 +0530

    add and narrowin and typeassertion in typescript

--- src/typesinTs.ts
+++ src/typesinTs.ts
@@ -15,3 +15,18 @@ console.log(userObject);
 
 // if i changes the in userobject  as user in place of number then it will give error because userobject is not of type user it is of type any so we have to forcefully assert the type to user using as user.
 // User her takes as string and we are trying to assign it to number so it will give error
+
+const inputElement = document.getElementById("username") as HTMLInputElement;
+
+// here we are forcefully asserting the type of inputElement to HTMLInputElement using as HTMLInputElement
+
+// because getElementById returns HTMLElement or null but we know that the element with id username is an input element so we are asserting it to HTMLInputElement
+
+let newValue: unknown;
+
+newValue = "Hello";
+newValue = 123;
+
+if (typeof newValue === "string") {
+  newValue.toUpperCase();
+}
